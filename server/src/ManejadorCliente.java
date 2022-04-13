631126d4338da651fc51247d2a517d3a52acd473
rickypazd <35882906+rickypazd@users.noreply.github.com>
2022-04-13 05:54:59 -0400

    create server

---
+++ server/src/ManejadorCliente.java
@@ -0,0 +1,32 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+import Servisofts.SConsole;
+
+public class ManejadorCliente {
+    public static void onMessage(JSONObject data, JSONObject config) {
+        if (data.isNull("component")) {
+            data.put("error", "No existe el componente");
+            return;
+        }
+        if (data.has("estado")) {
+            if (data.getString("estado").equals("error")) {
+                if (data.has("error")) {
+                    SConsole.log("ERROR: " + data.get("error").toString());
+                } else {
+                    SConsole.log("Error not found");
+                }
+            }
+        }
+
+        componentes(data, config);
+    }
+
+    public static void componentes(JSONObject data, JSONObject config) {
+        // switch (data.getString("component")) {
+        // case "usuario":
+        // usuario(data, config);
+        // break;
+        // }
+    }
+
+}

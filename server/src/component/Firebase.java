bafdf87b019d255c75d52d6193f254e940648dae
Roy Ruddy Paz Demiquel <ruddypazd@gmail.com>
2022-10-05 01:17:01 -0400

    cambiod

--- server/src/component/Firebase.java
+++ server/src/component/Firebase.java
@@ -6,11 +6,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class Firebase {
     
-    public static boolean send(String apiKeyServer, JSONObject message){
+    public static boolean _send(String apiKeyServer, JSONObject message){
         try{
 
             URL url = new URL("https://fcm.googleapis.com/fcm/send");
@@ -44,4 +45,30 @@ public class Firebase {
         }
     }
 
+    public static void send(String key_servicio, String title, String body){
+        
+            JSONObject fb_token = FirebaseToken.getAll(key_servicio);
+            String tokens_[] = JSONObject.getNames(fb_token);
+
+            JSONObject fb_server = FirebaseServer.getByKey(key_servicio);
+            fb_server = fb_server.getJSONObject(JSONObject.getNames(fb_server)[0]);
+
+            JSONObject message = new JSONObject();
+            message.put("priority", "high");
+            message.put("contentAvailable", true);
+            //
+
+            JSONObject notification = new JSONObject();
+            notification.put("title", title);
+            notification.put("body", body);
+            notification.put("sound", "default");
+
+            message.put("notification", notification);
+
+            for (int i = 0; i < tokens_.length; i++) {
+                message.put("to",  fb_token.getJSONObject(tokens_[i]).getString("token") );  
+                Firebase._send(fb_server.getString("key_server"),message);
+            }
+
+    }
 }
---
+++ server/src/component/FirebaseServer.java
@@ -0,0 +1,95 @@
+package component;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import Server.SSSAbstract.SSSessionAbstract;
+import Servisofts.SPGConect;
+import Servisofts.SUtil;
+
+public class FirebaseServer {
+    
+    public static final String COMPONENT = "firebase_server";
+
+    public static void onMessage(JSONObject obj, SSSessionAbstract session) {
+        switch (obj.getString("type")) {
+            case "getAll":
+                getAll(obj, session);
+                break;
+            case "registro":
+                registro(obj, session);
+                break;
+            case "send":
+                send(obj, session);
+                break;
+            case "editar":
+                editar(obj, session);
+                break;
+        }
+    }
+
+    public static void getAll(JSONObject obj, SSSessionAbstract session) {
+        try {
+            String consulta = "select get_all('" + COMPONENT + "') as json";
+            JSONObject data = SPGConect.ejecutarConsultaObject(consulta);
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            obj.put("error", e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONObject getByKey(String key_servicio) {
+        try {
+            String consulta = "select get_by_key('" + COMPONENT + "', 'key_Servicio' ,'"+key_servicio+"') as json";
+            return SPGConect.ejecutarConsultaObject(consulta);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static void registro(JSONObject obj, SSSessionAbstract session) {
+        try {
+            JSONObject data = obj.getJSONObject("data");
+            data.put("key", SUtil.uuid());
+            data.put("estado", 1);
+            data.put("fecha_on", SUtil.now());
+            data.put("key_usuario", obj.getString("key_usuario"));
+            SPGConect.insertArray(COMPONENT, new JSONArray().put(data));
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            obj.put("error", e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void send(JSONObject obj, SSSessionAbstract session) {
+        try {
+            JSONObject data = obj.getJSONObject("data");
+            Firebase.send(obj.getJSONObject("servicio").getString("key"), data.getString("title"), data.getString("body"));
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            obj.put("error", e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void editar(JSONObject obj, SSSessionAbstract session) {
+        try {
+            JSONObject data = obj.getJSONObject("data");
+            SPGConect.editObject(COMPONENT, data);
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (Exception e) {
+            obj.put("estado", "error");
+            obj.put("error", e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+}

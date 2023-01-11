9562dd17341791cd43fd0fce4af5d4b6d34c9413
Roy Ruddy Paz Demiquel <ruddypazd@gmail.com>
2023-01-11 02:39:13 -0400

    Cambios

---
+++ server/submit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scp ./server.jar servisofts@192.168.0.10:/home/servisofts/servicios/notification/entornos/notification/servicios/notification/
+#scp ./servisofts.jks servisofts@192.168.0.10:/home/servisofts/servicios/notification/entornos/notification/servicios/notification/
+#scp ./config.json servisofts@192.168.0.10:/home/servisofts/servicios/notification/entornos/notification/servicios/notification/
+#scp ./servicio.pem servisofts@192.168.0.10:/home/servisofts/servicios/notification/entornos/notification/servicios/notification/
